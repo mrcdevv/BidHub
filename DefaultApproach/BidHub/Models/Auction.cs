98bc90bf1ca3e0ecf2495f9defa63fc81b61611d
mrc <marcoemorales2015@gmail.com>
2023-11-14 23:39:05 -0300

    Model auction created

---
+++ DefaultApproach/BidHub/Models/Auction.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BidHub.Models
+{
+    public class Auction
+    {
+        public int Id { get; set; }
+        public int ReservePrice { get; set; }
+        public int Seller { get; set; }
+        public int Winner { get; set; }
+        public int SoldAmount { get; set; }
+        public int CurrentHighBid { get; set; }
+        public int CreatedAt { get; set; }
+        public int UpdatedAt { get; set; }
+        public int Status { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+    }
+}
